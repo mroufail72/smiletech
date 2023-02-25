722daf378e1e922291cd4f807f8e753e9da48567
Michelle <michelle.roufail@outlook.com>
2023-02-25 18:43:45 -0500

    Fix-4 Templates Not Found issue (Prod Only)

--- smile/settings.py
+++ smile/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
     'api.apps.ApiConfig',
-    'templates',
+    # 'templates',
 
 ]
 
@@ -131,14 +131,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
-
+]
 
 # If it doesn't work, declare your template path in a variable like
 # template_path = '../../' + BASE_DIR
